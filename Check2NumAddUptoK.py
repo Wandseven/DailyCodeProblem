5a71b37d1a55f9842cfafa016ab81b2bf3ad7d7c
Kiet <nguyentkiet.283@gmail.com>
2024-09-06 14:28:07 +0700

    update to random list and random k

--- Check2NumAddUptoK.py
+++ Check2NumAddUptoK.py
@@ -1,9 +1,16 @@
-list = [1,2,3,4,5,6,7]
-k =10
+import random
+
+#list = [1,2,3,4,5,6,7]
+#k = 10
+
+list = random.sample(range(1, 50), 10) # Generate a list of 10 random integers between 1 and 50 (inclusive)
+k = random.randint(1, 50) # Generate a random integer between 1 and 50 (inclusive)
+
 notFound = True
 
 # Check if any 2 numbers in the list add up to k
-
+print('Numbers in the list:', list)
+print('Target sum:', k)
 for i in range(len(list)):
     for j in range(i+1, len(list)):
         if list[i] + list[j] == k:
